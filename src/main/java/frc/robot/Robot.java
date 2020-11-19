89b8df7a16a5e2e1c44d300179e597f005c138a5
rahidas04 <56899753+rahidas04@users.noreply.github.com>
2020-11-19 17:32:24 -0600

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -16,6 +16,9 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -28,9 +31,8 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-
-  public static CANSparkMax HuddysSpark;
-
+  public static CANSparkMax RahiSpark;
+  public XboxController RahiController;
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
@@ -40,11 +42,9 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-
-    HuddysSpark = new CANSparkMax(1, MotorType.kBrushless);
-
-    HuddysSpark.setIdleMode(IdleMode.kBrake);
-    
+    RahiSpark = new CANSparkMax(1, MotorType.kBrushless);
+    RahiSpark.setIdleMode(IdleMode.kBrake);
+    RahiController = new XboxController(1);   
 
   }
 
@@ -58,7 +58,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    
   }
 
   /**
@@ -96,13 +95,39 @@ public class Robot extends TimedRobot {
   }
 
   /**
+   * This function is called once when teleop is enabled.
+   */
+  @Override
+  public void teleopInit() {
+  }
+
+  /**
    * This function is called periodically during operator control.
    */
   @Override
   public void teleopPeriodic() {
+    RahiSpark.set(RahiController.getTriggerAxis(Hand.kRight));
+  }
 
-    HuddysSpark.set(0);
-  
+  /**
+   * This function is called once when the robot is disabled.
+   */
+  @Override
+  public void disabledInit() {
+  }
+
+  /**
+   * This function is called periodically when disabled.
+   */
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  /**
+   * This function is called once when test mode is enabled.
+   */
+  @Override
+  public void testInit() {
   }
 
   /**
