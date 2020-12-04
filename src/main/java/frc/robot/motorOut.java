21a3fab5abe6eb0acf44cf1003a68d9fcc686b6e
Andrew S. Kepych <613ask11@stu.olatheschools.org>
2020-12-03 19:39:17 -0600

    Some PID Calculation Added

--- src/main/java/frc/robot/motorOut.java
+++ src/main/java/frc/robot/motorOut.java
@@ -12,12 +12,14 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.XboxController;
+import java.math.BigDecimal;
 /**
  * Everything related to the motor
  */
 public class motorOut {
     public static CANSparkMax Spark;
     private static XboxController xboxController = new XboxController(1);
+    private static BigDecimal Adegree = new BigDecimal(180.0);
 
     public static void motorInit() {
         Spark = new CANSparkMax(4, MotorType.kBrushless);
@@ -26,22 +28,39 @@ public class motorOut {
     }
 
     public static void setPosition(){
-        //TODO: add calculation for shortest route
+        BigDecimal Position = new BigDecimal(getPosition());
+        BigDecimal Distance = new BigDecimal(0);
+        BigDecimal k = new BigDecimal(.01);
+        BigDecimal Speed = new BigDecimal(0);
+        double speed;
+        //This should all work in theory
         if(xboxController.getAButton() && getPosition() != 180){
-            Spark.set(.6);
+            Distance = Adegree.subtract(Position);
+            Speed = Distance.multiply(k);
+            speed = Speed.doubleValue();
+            Spark.set(speed);
         }
         else if(xboxController.getBButton() && getPosition() != 90){
-            Spark.set(.6);
+            Distance = Adegree.subtract(Position);
+            Speed = Distance.multiply(k);
+            speed = Speed.doubleValue();
+            Spark.set(speed);
         }
         else if(xboxController.getXButton() && getPosition() != 270){
-            Spark.set(.6);
+            Distance = Adegree.subtract(Position);
+            Speed = Distance.multiply(k);
+            speed = Speed.doubleValue();
+            Spark.set(speed);
         }
         else if(xboxController.getYButton() && getPosition() != 0){
-            Spark.set(.6);
+            Distance = Adegree.subtract(Position);
+            Speed = Distance.multiply(k);
+            speed = Speed.doubleValue();
+            Spark.set(speed);
         }
         else{
             Spark.set(.0);
-        } //All this looks too simple for the time I spent on it
+        } 
     }
     
     public static double getPosition(){
