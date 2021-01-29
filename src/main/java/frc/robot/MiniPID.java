8de72551e0cffc7e823b12893f886dec64864ac2
rahidas04 <56899753+rahidas04@users.noreply.github.com>
2021-01-28 19:04:46 -0600

    pushed minipid file

---
+++ src/main/java/frc/robot/MiniPID.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class MiniPID {
+
+	public MiniPID(double kP, double kI, double kD) {
+	}
+
+	public double getOutput(double velocity) {
+		return 0;
+	}
+
+	public void reset() {
+	}
+
+}
